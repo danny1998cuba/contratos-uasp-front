53862a2e3b40c961cba685236b3d90a983c7cb7c
Daniel González Cuétara <danny.glezcuet98@gmail.com>
2022-03-22 20:57:55 -0400

    Role Changed

--- src/app/data/constants/icard.const.ts
+++ src/app/data/constants/icard.const.ts
@@ -7,7 +7,7 @@ export const CARDS_INFO: ICardData[] = [
         description: "Gestión de los proveedores de la Unidad",
         router_Link: "/gestion/providers",
         image: "assets/images/provider.png",
-        small: "Solo para usuarios de tipo Contratador",
+        small: "Solo para usuarios de tipo Planificador",
         enabled: "CONT"
     },
     {
@@ -15,7 +15,7 @@ export const CARDS_INFO: ICardData[] = [
         description: "Gestión de los contratos establecidos por la Unidad",
         router_Link: "/gestion/contracts",
         image: "assets/images/contract2.png",
-        small: "Solo para usuarios de tipo Contratador",
+        small: "Solo para usuarios de tipo Planificador",
         enabled: "CONT"
     },
     {
